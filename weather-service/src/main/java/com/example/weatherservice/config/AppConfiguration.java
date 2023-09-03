daa944b630e1b90348b8e91251f87600b65cebfe
mihailco <mihail.egorov.03@list.ru>
2023-09-03 19:09:49 +0300

    fix cache

--- weather-service/src/main/java/com/example/weatherservice/config/AppConfiguration.java
+++ weather-service/src/main/java/com/example/weatherservice/config/AppConfiguration.java
@@ -1,10 +1,17 @@
 package com.example.weatherservice.config;
 
+import com.google.common.cache.CacheBuilder;
+import lombok.NonNull;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class AppConfiguration {
@@ -12,5 +19,20 @@ public class AppConfiguration {
     @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean("habrCacheManager")
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager() {
+            @Override
+            protected @NonNull Cache createConcurrentMapCache(String name) {
+                return new ConcurrentMapCache(
+                        name,
+                        CacheBuilder.newBuilder()
+                                .expireAfterWrite(1, TimeUnit.MINUTES)
+                                .build().asMap(),
+                        false);
+            }
+        };
     }
 }
--- weather-service/src/main/java/com/example/weatherservice/service/WeatherService.java
+++ weather-service/src/main/java/com/example/weatherservice/service/WeatherService.java
@@ -15,6 +15,8 @@ public class WeatherService {
     @Autowired
     RestTemplate restTemplate;
 
+    RestTemplate externalRestTemplate = new RestTemplate();
+
     @Value("${appid}")
     String appId;
 
@@ -23,16 +25,18 @@ public class WeatherService {
 
     @Cacheable(value = "weathers", key = "#lon + ';' + #lat")
     public Main getWeatherByCord(String lon, String lat){
+
         String url = String.format("%s?lat=%s&lon=%s&units=metric&appid=%s", weatherUrl, lat, lon, appId);
         Root t = null;
         try {
-            t = restTemplate.getForObject(url, Root.class);
+            t = externalRestTemplate.getForObject(url, Root.class);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
         return t.getMain();
     }
 
+    @Cacheable(value = "weathers", key = "#cityName")
     public Main getWeatherByCityName(String cityName) {
         String urlLocatorService = String.format("%s?city=%s", ServicesUrl.locationService, cityName);
        var t = restTemplate.getForObject(urlLocatorService, Geodata.class);
